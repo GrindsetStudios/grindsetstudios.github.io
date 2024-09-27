e78d7b57119898a62be44ed106d52401c84d6242
Defstok <Defstok@gmail.com>
2024-09-27 22:50:08 +0500

    GrindsetSite_eleventy_add

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+    // Копирование статических файлов из указанных папок
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy("scripts");
+
+    // Вернуть конфигурацию
+    return {
+        dir: {
+            input: ".", // папка с исходниками (по умолчанию)
+            output: "_site", // папка для выходных файлов
+        }
+    };
+};
